67b9bd0fc85c64511c498bbe5ca65e53575a5c98
Ramses <sesmar111@hotmail.com>
2024-06-07 21:55:10 -0700

    Adding test java file

---
+++ Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String [] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Hellow World!");
+        System.out.println("How old are you?");
+        if (scn.nextInt() >= 18)
+            System.out.println("You are an adult!");
+        else
+            System.out.println("You are under age");
+        Sys
+    }
+
+
+}
